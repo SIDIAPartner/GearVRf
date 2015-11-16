5984465fa50a62edbe5cbb0ef473a29dd52a2b63
Sidia Partner <sidia_partner@fpf.br>
2015-11-16 10:53:09 -0400

    android gradle build configuration

---
+++ GVRf/Framework/androidTest/java/org/gearvrf/GVRActivityTest.java
@@ -0,0 +1,13 @@
+package org.gearvrf;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by isandro.azedo on 12/11/2015.
+ */
+public class GVRActivityTest extends AndroidTestCase {
+
+    public void testNativeSetAppInterface() throws Exception {
+        // TODO: 12/11/2015
+    }
+}
---
+++ GVRf/Framework/unitTest/java/org/gearvrf/animation/GVRAccelerateDecelerateInterpolatorTest.java
@@ -0,0 +1,13 @@
+package org.gearvrf.animation;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by isandro.azedo on 12/11/2015.
+ */
+public class GVRAccelerateDecelerateInterpolatorTest extends TestCase {
+
+    public void testMapRatio() throws Exception {
+        // TODO: 12/11/2015
+    }
+}
