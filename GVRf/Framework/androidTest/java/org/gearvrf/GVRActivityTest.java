53d8dff00de59766f31d71970a93480f2f116804
Sidia Partner <sidia_partner@fpf.br>
2015-11-23 16:46:42 -0400

    Removing class header

--- GVRf/Framework/androidTest/java/org/gearvrf/GVRActivityTest.java
+++ GVRf/Framework/androidTest/java/org/gearvrf/GVRActivityTest.java
@@ -2,9 +2,6 @@ package org.gearvrf;
 
 import android.test.AndroidTestCase;
 
-/**
- * Created by isandro.azedo on 12/11/2015.
- */
 public class GVRActivityTest extends AndroidTestCase {
 
     public void testNativeSetAppInterface() throws Exception {
